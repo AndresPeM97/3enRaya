ebd415ab2e74b0be9831577db1128d8574a5d3bb
AndresPeM97 <andresprezmartinez@hotmail.com>
2024-06-02 21:17:54 -0600

    Correccion de bug

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 const saberGanador = (newBoard) => {
     let encontrado = null
     newBoard.forEach((casilla, index) => {
-        let i = (index === 2 || index === 3 || index == 4 || index === 5) ? 2 : 1 
+        let i = (index === 2 && index === 5) ? 2: 1
+            i = (index === 3) ? 3 : i
         let posicion = 0
         while (encontrado === null && i <= 4 && casilla !== null){
             if(casilla === newBoard[index + i]){

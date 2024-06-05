d6c5c2d416785ec55bd0b730afd503074f88f713
AndresPeM97 <andresprezmartinez@hotmail.com>
2024-06-04 20:22:15 -0600

    Correccion de bug

--- src/application/scripts.js
+++ src/application/scripts.js
@@ -1,24 +1,18 @@
+import { opciones } from "./constants";
+
 export const saberGanador = (newBoard) => {
-    let encontrado = null
-    newBoard.forEach((casilla, index) => {
-        let i = (index === 2 && index === 5) ? 2: 1
-            i = (index === 3 ) ? 3 : i
-        let posicion = 0
-        while (encontrado === null && i <= 4 && casilla !== null){
-            if(casilla === newBoard[index + i]){
 
-                posicion = i
-                console.log(posicion)
-            }
 
-            if(posicion !== 0 && casilla === newBoard[index - posicion] && index !== 2 && (index !== 5 || (index === 5 && i === 3))){
-
-                console.log(casilla)
-                encontrado = casilla
-            }
-            i++
+    for (const opcion of opciones){
+        const [a, b, c] = opcion
+        if(
+            newBoard[a] &&
+            newBoard[a] === newBoard[b] &&
+            newBoard[a] === newBoard[c]
+        ){
+            return newBoard[a]
         }
-    });
+    }
 
-    return encontrado
+    return null
 }
